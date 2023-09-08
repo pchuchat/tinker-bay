0fa6b331dd4b1169b293e31772af6efd81fce512
Phatchanon Chuchat <pchuchat@student.jyu.fi>
2023-09-08 20:24:10 +0300

    frondend working

---
+++ app/layouts/MainLayout.js
@@ -0,0 +1,21 @@
+"use client";
+import Footer from "./includes/Footer";
+import MainHeader from "./includes/MainHeader";
+import SubMenu from "./includes/SubMenu";
+import TopMenu from "./includes/TopMenu";
+
+export default function MainLayout({children}) {
+    return (
+        <>
+            <div id="MainLayout" className="min-w-[1050px] max-w-[1300px] mx-auto">
+                <div>
+                    <TopMenu />
+                    <MainHeader />
+                    <SubMenu />
+                    <div>{children}</div>
+                    <Footer />
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ app/layouts/includes/Footer.js
@@ -0,0 +1,17 @@
+"use client"
+
+
+export default function Footer() {
+    return(
+       
+        <footer id="Footer" className="border-t mt-20 px-2">
+            <div className="flex items-baseline justify-between w-full mx-auto max-w-[1200px] py-10">
+                <p className="font-bold text-center text-sm text-black">
+                    &copy; 2023 TinkerIT Oy. All rights reserverd.
+                </p>
+
+            </div>
+            
+        </footer>
+    )
+}
