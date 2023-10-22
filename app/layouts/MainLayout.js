5af3724af3702cefa98f5e9f45318a85cf5c7537
Phatchanon Chuchat <pchuchat@student.jyu.fi>
2023-10-22 18:52:56 +0300

    Almost done.

--- app/layouts/MainLayout.js
+++ app/layouts/MainLayout.js
@@ -3,12 +3,24 @@ import Footer from "./includes/Footer";
 import MainHeader from "./includes/MainHeader";
 import SubMenu from "./includes/SubMenu";
 import TopMenu from "./includes/TopMenu";
+import Loading from '../components/Loading'
+import { useEffect, useState } from 'react'
 
 export default function MainLayout({children}) {
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() =>{
+        window.addEventListener("storage", function () {
+          let res = localStorage.getItem('isLoading')
+          res === 'false' ? setIsLoading(false) : setIsLoading(true)
+        })
+      })
+  
     return (
         <>
             <div id="MainLayout" className="min-w-[1050px] max-w-[1300px] mx-auto">
                 <div>
+                    {isLoading ? <Loading /> : <div></div>}
                     <TopMenu />
                     <MainHeader />
                     <SubMenu />
