109c01424aa4a738ada3d062d82224dfaaf779c5
pchuchat <pchuchat@student.jyu.fi>
2023-10-23 14:21:54 +0300

    finnish

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -2,9 +2,138 @@
 
 import CheckoutItem from "../components/CheckoutItem"
 import MainLayout from "../layouts/MainLayout"
-
+import Link from "next/link"
+import { useUser } from "@/app/context/user";
+import { useCart } from "../context/cart";
+import { useEffect, useRef, useState } from "react";
+import useIsLoading from "../hooks/useIsLoading";
+import useUserAddress from "../hooks/useUserAddress";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import ClientOnly from "../components/ClientOnly";
+import { loadStripe } from '@stripe/stripe-js';
 
 export default function Checkout() {
+
+    const user = useUser();
+    const cart = useCart();
+    const router = useRouter()
+
+    let stripe = useRef(null)
+    let elements = useRef(null)
+    let card = useRef(null)
+    let clientSecret = useRef(null)
+
+    const [addressDetails, setAddressDetails] = useState({})
+    const [isLoadingAddress, setIsLoadingAddress] = useState(false)
+
+    useEffect(() => {
+        if (cart?.cartTotal() <= 0) {
+            toast.error("Your cart is empty!", { autoClose: 3000 })
+            return router.push('/')
+        }
+
+        useIsLoading(true)
+
+        const getAdddress = async () => {
+            if (user?.id == null || user?.id == undefined) {
+                useIsLoading(false)
+                return
+            }
+
+            setIsLoadingAddress(true)
+            const response = await useUserAddress()
+            if (response) setAddressDetails(response)
+            setIsLoadingAddress(false)
+        }
+
+        getAdddress()
+        setTimeout(() => stripeInit(), 300)
+    }, [user])
+
+    const stripeInit = async () => {
+        stripe.current = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PK_KEY || '');
+    
+        const response = await fetch('/api/stripe', {
+            method: 'POST',
+            body: JSON.stringify({ amount: cart.cartTotal() })
+        })
+        const result = await response.json()
+
+        clientSecret.current = result.client_secret
+        elements.current = stripe.current.elements();
+        var style = {
+            base: { fontSize: "18px" },
+            invalid: {
+                fontFamily: 'Arial, sans-serif',
+                color: "#EE4B2B",
+                iconColor: "#EE4B2B"
+            }
+        };
+        card.current = elements.current.create("card", {  hidePostalCode: true, style: style });
+    
+        card.current.mount("#card-element");
+        card.current.on("change", function (event) {
+            document.querySelector("button").disabled = event.empty;
+            document.querySelector("#card-error").textContent = event.error ? event.error.message : "";
+        });
+
+        useIsLoading(false)
+    }
+
+    const pay = async (event) => {
+        event.preventDefault()
+
+        if (Object.entries(addressDetails).length == 0) {
+            showError('Please add shipping address!')
+            return 
+        }
+        
+        let result = await stripe.current.confirmCardPayment(clientSecret.current, {
+            payment_method: { card: card.current },
+        })
+    
+        if (result.error) {
+            showError(result.error.message)
+        } else {
+            useIsLoading(true)
+
+            try {
+                let response = await fetch('/api/orders/create', {
+                    method: "POST",
+                    body: JSON.stringify({
+                        stripe_id: result.paymentIntent.id,
+                        name: addressDetails.name,
+                        address: addressDetails.address,
+                        zipcode: addressDetails.zipcode,
+                        city: addressDetails.city,
+                        country: addressDetails.country,
+                        products: cart.getCart(),
+                        total: cart.cartTotal()
+                    })
+                })
+                
+                if (response.status == 200) {
+                    toast.success('Order Complete!', { autoClose: 3000 })
+                    cart.clearCart()
+                    return router.push('/success')
+                }
+            } catch (error) {
+                console.log(error)
+                toast.error('Something went wrong?', { autoClose: 3000 })
+            }
+
+            useIsLoading(false)
+        }
+    }
+
+    const showError = (errorMsgText) => {
+        let errorMsg = document.querySelector("#card-error");
+        toast.error(errorMsgText, { autoClose: 3000 })
+        errorMsg.textContent = errorMsgText;
+        setTimeout(() => { errorMsg.textContent = "" }, 3000);
+    };
 
     
   return (
@@ -99,7 +228,7 @@ export default function Checkout() {
                         </ClientOnly>
 
                         <div className="flex items-center p-4 justify-center gap-2 border-t">
-                            <img width={50} src="/images/logo.svg" />
+                            <img width={50} src="/images/Tinkerit.svg" />
                             <div className=" font-light mb-2 mt-2">MONEY BACK GUARANTEE</div>
                         </div>
                     </div>
